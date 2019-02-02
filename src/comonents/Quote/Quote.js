5b4e0d2ef0455448636991b93fb86b75207783eb
mkubibaev <mkubibaev@gmail.com>
2019-02-02 13:51:03 +0600

    added quote component

---
+++ src/comonents/Quote/Quote.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Quote = props => {
+    return (
+        <div className="card mb-3">
+            <div className="card-body">
+                <p><i>{props.quote}</i></p>
+                <span>- {props.author}</span>
+            </div>
+            <div className="card-footer text-right">
+                <button type="button" className="btn btn-sm btn-secondary mr-1">Edit</button>
+                <button type="button" className="btn btn-sm btn-danger">Delete</button>
+            </div>
+        </div>
+    );
+};
+
+export default Quote;
--- src/containers/QuotesList/QuotesList.js
+++ src/containers/QuotesList/QuotesList.js
@@ -1,10 +1,45 @@
 import React, {Component} from 'react';
+import axios from '../../axios-quotes';
+import Quote from "../../comonents/Quote/Quote";
 
 class QuotesList extends Component {
+    state = {
+        quotes: null
+    };
+
+    loadData() {
+        axios.get('/quotes.json').then(response => {
+            const quotes = Object.keys(response.data).map(id => {
+                return {...response.data[id], id}
+            });
+
+            this.setState({quotes});
+        })
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
     render() {
+        let quotes = null;
+
+        if (this.state.quotes) {
+            quotes = this.state.quotes.map(quote => (
+                <Quote key={quote.id} quote={quote.quote} author={quote.author}/>
+            ))
+        }
+
         return (
-            <div>
-                
+            <div className="container py-3">
+                <div className="row">
+                    <div className="col-12 col-md-4">
+                        <div>categories</div>
+                    </div>
+                    <div className="col-12 col-md-8">
+                        {quotes}
+                    </div>
+                </div>
             </div>
         );
     }
