26692e3bc287173b453f1d029648d68b459a5e7d
mkubibaev <mkubibaev@gmail.com>
2019-02-02 11:52:24 +0600

    added Header component with router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Header from "./comonents/Header/Header";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <Fragment>
+                    <Header/>
+                    <Switch>
+                        <Route path='/' exact/>
+                        <Route path='/add' exact render={() => <h1>add</h1>}/>
+                    </Switch>
+                </Fragment>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
---
+++ src/comonents/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const Header = props => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <NavLink to="/" className="navbar-brand">Quotes Central</NavLink>
+            <ul className="nav">
+                <li className="nav-item">
+                    <NavLink to="/" exact className="nav-link">Quotes</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/add" className="nav-link">Submit new quote</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
